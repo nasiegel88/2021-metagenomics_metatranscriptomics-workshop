c32357df40013cc2924f0726a580666e5a5f9908
nasiegel88 <nasiegel@ucdavis.edu>
2021-12-14 10:46:04 -0800

    Add dependencies and renv environment

---
+++ R/dependencies.R
@@ -0,0 +1,58 @@
+# packages
+options(repos="https://CRAN.R-project.org")
+
+if(!require(renv)){
+install.packages("renv")
+library('renv')
+}
+
+if(!require(devtools)){
+  install.packages("devtools")
+  library('devtools')
+}
+
+# bioconductor
+if(!require(BiocManager)){
+  install.packages("BiocManager")
+  library('BiocManager')
+}
+
+if(!require(kableExtra)){
+  install.packages("kableExtra")
+  library('kableExtra')
+}
+
+if(!require(qqman)){
+  install_github("stephenturner/qqman") # requires devtools to install
+  library('qqman')
+}
+
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library('dplyr')
+}
+
+if(!require(knitr)){
+  install.packages("knitr")
+  library('knitr')
+}
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library('tidyverse')
+}
+
+if(!require(renv)){
+  install.packages("renv")
+  library('renv')
+}
+
+if(!require(bsseq)){
+  BiocManager::install("bsseq")
+  library('bsseq')
+}
+
+if(!require(DSS)){
+  BiocManager::install("DSS", force = T)
+  library('DSS')
+}
